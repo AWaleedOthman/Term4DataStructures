791d3205dedb8a70d4d0452765f911c5b2d4b005
Ahmad Waleed <a.waleedothman@gmail.com>
2020-02-19 16:21:05 +0200

    added test cases

--- Calculator,Lab1/src/MyCalculatorTest.java
+++ Calculator,Lab1/src/MyCalculatorTest.java
@@ -5,16 +5,17 @@ class MyCalculatorTest {
     @org.junit.jupiter.api.Test
     void add() {
         MyCalculator calc = new MyCalculator();
-            assertEquals(-1, calc.add(-1,0));
+        assertEquals(-1, calc.add(-1, 0));
     }
 
     @org.junit.jupiter.api.Test
     void divide() {
         MyCalculator calc = new MyCalculator();
         try {
-            float output = calc.divide(6, -1);
-            assertEquals(-6, output);
-        }catch (RuntimeException e) {
+            assertEquals(-6, calc.divide(6, -1));
+            assertEquals(2.5, calc.divide(5, 2));
+            assertEquals(0, calc.divide(5, 0));
+        } catch (RuntimeException e) {
             System.out.println("ERROR: division by zero");
         }
     }
